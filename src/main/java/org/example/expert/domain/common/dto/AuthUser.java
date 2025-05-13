f3b53e86519ff289479baa40554870ebcbdd944d
Kimyoonbeom <kimybeom@naver.com>
2025-05-13 20:42:12 +0900

    9. Spring Security-refactor
    AuthUser에 UserDetails 추가,
    컨트롤러 수정.

--- src/main/java/org/example/expert/domain/common/dto/AuthUser.java
+++ src/main/java/org/example/expert/domain/common/dto/AuthUser.java
@@ -2,9 +2,15 @@ package org.example.expert.domain.common.dto;
 
 import lombok.Getter;
 import org.example.expert.domain.user.enums.UserRole;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 @Getter
-public class AuthUser {
+public class AuthUser implements UserDetails {
 
     private final Long id;
     private final String email;
@@ -17,4 +23,16 @@ public class AuthUser {
         this.nickname = nickname;
         this.userRole = userRole;
     }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_" + userRole.name()));
+    }
+
+    @Override public String getPassword() { return null; }
+    @Override public String getUsername() { return email; }
+    @Override public boolean isAccountNonExpired() { return true; }
+    @Override public boolean isAccountNonLocked() { return true; }
+    @Override public boolean isCredentialsNonExpired() { return true; }
+    @Override public boolean isEnabled() { return true; }
 }
